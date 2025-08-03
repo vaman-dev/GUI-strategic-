a471a259e570e2f15f171834a0b90472ffc16f5b
Apoorva <apoorvas754sharma@gmail.com>
2025-08-03 17:29:50 +0530

    changes

--- Assets/Scripts/CityDatabase.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using System.Collections.Generic;
-
-[CreateAssetMenu(fileName = "CityDatabase", menuName = "Map/CityDatabase", order = 2)]
-public class CityDatabase : ScriptableObject
-{
-    public List<CityLocation> cities;
-}
--- Assets/Scripts/CityLocation.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CityLocation", menuName = "Map/CityLocation", order = 1)]
-public class CityLocation : ScriptableObject
-{
-    public string cityName;
-    public float latitude;
-    public float longitude;
-}
